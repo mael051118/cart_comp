3e6d1fc7bc89e5d473e6e0fec1a8b781c64afa5e
unknown <manuellazarte28@gmail.com>
2023-06-29 23:45:22 -0500

    sub1

---
+++ src/main/java/com/cart/comp/model/Orden.java
@@ -0,0 +1,5 @@
+package com.cart.comp.model;
+
+public class Orden {
+
+}
---
+++ src/test/java/com/cart/comp/CartCompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cart.comp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartCompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
